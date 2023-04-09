c4803532a46463a2300bad7f7d84e24994ddfef4
mzxdream <mzxdream@gmail.com>
2023-04-09 21:34:32 +0800

    1

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform tomatoPrefab;
+
     public void Interact()
     {
         Debug.LogErrorFormat("{0} interact", gameObject.name);
+        var tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
